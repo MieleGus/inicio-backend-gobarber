63f1e924e23c1f168a69309ade017d584d1d94a5
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-08-05 13:42:24 -0300

    Arquitetura DDD

---
+++ src/shared/container/index.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+
+import IAppointmentsRepositoy from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
+import IUsersRepositoy from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+container.registerSingleton<IAppointmentsRepositoy>(
+    'AppointmentsRepository',
+    AppointmentsRepository,
+);
+
+container.registerSingleton<IUsersRepositoy>(
+    'UsersRepository',
+    UsersRepository,
+);
